8642bfacd008c73dd890e03c43886e36dd23adc0
Joshua Bowers <joshua.bowers+code@gmail.com>
2013-09-30 20:22:19 -0700

    Initializing project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wanderlust::Application.config.secret_key_base = 'dedf2610a4de9b6a5cade8d711b30a89e2eb50f23a84193f6233c53bc03ea50d841d1c3f624210af58b19edf96b41033c9f4f197ecdb195855df3f4a224f6af4'
